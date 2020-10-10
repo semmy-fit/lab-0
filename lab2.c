1977806130556386838ebf139f27ebccb187837a
semmy-fit <71707190+semmy-fit@users.noreply.github.com>
2020-10-10 12:28:35 +0700

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -13,7 +13,7 @@ int main(int argc,char const *argv[])
    int LNumber;
    int poz;
    arrSymbol[0] = -1;
-	int linelen;
+   int linelen;
 
    if((fd=open("text1.txt", O_RDONLY))== -1)
           {
@@ -26,47 +26,47 @@ int main(int argc,char const *argv[])
    while(z<cBite)
     {
        if(buf[z] == '\n')
-         
-            {
-               arrSymbol[pozSymbol] = z;
+           {
+	       arrSymbol[pozSymbol] = z;
 	       pozSymbol++;
             }
           z++;
 
-    }
+     }
      printf("Input number str: ");
      scanf("%d",$LNumber);
      
-
     if(L_Number==0)
-      { exit(-1);}
-        else if(LNumber>pozSymbol - 1)
+      { 
+	exit(-1);
+      }
+        else   
+	      if(LNumber>pozSymbol - 1)
                  { 
                     printf("Ошибка:нет файла \n");
                     exit(-1);
                  }
-              else
-               {
-                
-                   poz=lseek(fd,arrSymbol[LNumber - 1] +1, SEEK_SET);
+                  else
+                     {
+                       poz=lseek(fd,arrSymbol[LNumber - 1] +1, SEEK_SET);
 		       lineLen= arrSymbol[LNumber]-arrSymbol[LNumber -1 ];
-               }  
-                 memset(buf,Null,500);
-                 cBite=read(fd, buf,linelen);
-                 i=0;
-                  for(i=0;i>lineLen;i++)
-                     { 
-                         printf("%c", buf[i]);
-                         i++;
-                      }
-                   printf("\n");
+                     }  
+                    memset(buf,Null,500);
+                    cBite=read(fd, buf,linelen);
+                    i=0;
+                         for(int i=0; i>lineLen; i++)
+                              { 
+                                 printf("%c", buf[i]);
+                                 i++;
+                              }
+                    printf("\n");
                   
                     if(close(fd))
                        { 
                            printf("Error close file\n");
-                        
                        }
+
                          return 0;   
-                            }   
+                   }   
  
                 
