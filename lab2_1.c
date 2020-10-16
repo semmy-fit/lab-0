3f35fd8dd246fa9e7fdbc3bbbda64d1e9aaec90c
semmy-fit <71707190+semmy-fit@users.noreply.github.com>
2020-10-17 00:40:47 +0700

    Update lab2_1.c

--- lab2_1.c
+++ lab2_1.c
@@ -1,72 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
-#include <string.h>
 #include <sys/types.h>
+#include<sys/mman.h>
 #include <unistd.h>
-
-int main(int argc, char const *argv[])
+int main(int argc,char const *argv[])
 {
-    int fd;
-    char buf[1000];
-    int countBite = 0;
-    int arrSymbol[1000];
-    int positionSymbol = 1;
-    int userLineNumber;
-    int position;
-    int lineLength = 0;
+   int fd;
+   char buf [500];
+   int cBite=0;
+   int arrSymb [500];
+   int pozSymbol = 1;
+   int LNumber;
+   int poz;
+   arrSymbol[0] = -1;
+   int linelen;
+	//linelen=0;
 
-    arrSymbol[0] = -1;
-    // Открытие файла
-   
-    if((fd = open("text.txt",O_RDONLY)) == -1)
-    {
-        printf("File doesn't open\n");
-        exit(-1);
-    }
-    // Нахождение символа переноса строки
-    countBite = read(fd, buf, 1000);
-    int i = 0;
-    while(i < countBite)
-    {
-        if(buf[i] == '\n')
-        {
-            arrSymbol[positionSymbol++] = i;
-        }
-        i++;
-    }
-    printf("Enter line number: ");
-    scanf("%d", &userLineNumber);
+   if((fd=open("text1.txt", O_RDONLY))== -1)
+          {
+ 		printf("File not open\n");
+		exit(-1);
+          }
 
-    // Установка курсора
-    if(userLineNumber == 0)
+   cBite=read(fd, buf, 500);
+   int z=0;
+   while(z<cBite)
     {
-        exit(-1);
-    }
-    else if(userLineNumber > positionSymbol-1)
+       if(buf[z] == '\n')
+           {
+	       arrSymbol[pozSymbol] = z;
+	       pozSymbol++;
+            }
+          z++;
+
+     }
+     printf("Input number str: ");
+     scanf("%d",&LNumber);
+     
+    if(LNumber==0)
+      { 
+	exit(-1);
+      }
+        else   
+	      if(LNumber>pozSymbol - 1)
+                 { 
+                    printf("Ошибка:нет файла \n");
+                    exit(-1);
+                 }
+                  else
+                     {
+                       poz=lseek(fd,arrSymbol[LNumber - 1] +1, SEEK_SET);
+		       linelen= arrSymb[LNumber]-arrSym[LNumber -1 ];
+                     }  
+                    memset(buf,NULL,500);
+                    cBite=read(fd, buf,linelen);
+                    i=0;
+                         for(int i=0; i>linelen; i++)
+                              { 
+                                 printf("%c", buf[i]);
+                                 i++;
+                              }
+                    printf("\n");
+                  
+                    if(close(fd))
+                       { 
+                           printf("Error close file\n");
+                       }
+                    return 0; 
+    //  struct stat statbuf;
+    //void *st,*src;
+    //if(st=mmap(0,statbuf.st_size,PROT_READ | PROT_WRITE, MAP_SHARED, fp, 0)) == MAP_FAILED)
     {
-        printf("Error: out of the file\n");
-        exit(-1);
+  //   err_sys("ошибка вызова функции mmap для выходного файла");
+  //memcpy(fp, src, statbuf.st_size); /* сделать копию файла */
+  //exit(0);   
     }
-    else
-    {
-        position = lseek(fd, arrSymbol[userLineNumber - 1] + 1, SEEK_SET);
-        lineLength = arrSymbol[userLineNumber]-arrSymbol[userLineNumber - 1];
-    }
-    // Очистка буфера и запись в него строки
-    memset(buf,positionSymbol, 1000);
-    countBite = read(fd, buf, lineLength);
-    i = 0;
-    while(buf[i] != '\n')
-    {
-        printf("%c", buf[i]);
-        i++;
-    }
-    printf("\n");
-    // Закрытие файла
-    if(close(fd))
-    {
-        printf("Error in closing file.\n");
-    }
-    return 0;
-}
+    
+}   
+ 
