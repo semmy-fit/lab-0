9d456b88cb2ec5cb6b0dc157b6722689ee67ff4c
semmy-fit <71707190+semmy-fit@users.noreply.github.com>
2020-11-07 11:24:57 +0700

    Update analog_ls.c

--- analog_ls.c
+++ analog_ls.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+
+ #include <stdio.h>
 #include <dirent.h>
 #include <string.h>
 #include <pwd.h>
@@ -14,16 +15,16 @@ int main(int argc, char **argv)
     struct dirent *dp;
     char filename[NAME_MAX];
  
-    if ( argc < 2 )
-        strcpy(filename, ".");
+    if ( argc < 2 ) {strcpy(filename, ".");}
     else
-        strcpy(filename, argv[1]);
+       { strcpy(filename, argv[1]);}
  
     printf("%s\n\n", filename);
     dfd=opendir(filename);
  
     while( (dp=readdir(dfd)) != NULL )
-            printf("%s\n", dp->d_name);
+          
+          {  printf("%s\n", dp->d_name);}
      if(filename=="")
      {
        
@@ -36,37 +37,36 @@ int main(int argc, char **argv)
   struct group *grp;
 
 if ((grp = getgrgid(statbuf.st_gid)) != NULL)
-    printf(" %-8.8s", grp->gr_name);
+  {   printf(" %-8.8s", grp->gr_name);}
 else
-    printf(" %-8d", statbuf.st_gid);
+ {   printf(" %-8d", statbuf.st_gid);
     
     closedir(dfd);
-    return 0;
 }
-
- int typeOf(char *name)
-    {  
+    
+     char *name;
+     
        int type; 
        struct stat st;
-       if( stat( name, &st ) < 0 ){
+       if( stat(name, &st ) < 0 ){
                printf( "%s не существует\n", name );
                return 0;
        }
-       printf("Файл имеет %d имен\n", st.st_nlink);
-       struct tm *timeinfo=localtime(&st.st_mtime)
+       printf("Файл имеет %d имен\n",st.st_nlink);
+       struct tm *timeinfo=localtime(st.st_mtime);
        int ttime;
 
        // Считываем текущее время
-        ctime = ttime (NULL);
+        ttime = ctime (NULL);
 
         // С помощью функции ctime преобразуем считанное время в
         // локальное, а затем в строку и выводим в консоль.
-        printf ("Время: %s\n",ctime (&ctime) );
+        printf ("Время: %s\n",&time);
          
        //type=(st.st_mode & S_IFMT);
   
        if(st.st_mode & S_IFREG)  { printf( "Обычный файл размером %ld байт\n", st.st_size );}                
-       if( st.st_mode & S_IFDIR  { printf( "Каталог\n",st.st_size );}   
+       if( st.st_mode & S_IFDIR)  { printf( "Каталог\n",st.st_size );}   
        if(st.st_mode & S_IFCHR) { printf("",st.st_size ); }
        if(st.st_mode & S_IFBLK)  { printf( "Устройство\n",st.st_size); } 
        if (st.st_mode  & S_IFIFO { printf( "FIFO-файл\n",st.st_size ); }
@@ -90,6 +90,13 @@ else
                 strcat(Other,"x"); 
               } 
                else
-                 { strcat(Other,"Обычный файл"); }        
+                 { 
+                 strcat(Other,"Обычный файл"); 
+                 }   
+    
+    
+    
+    return 0;
+}
+
      
-  }
