c3a95d204aaf8c14c6ef1e7b4d7d3b5c6e80589b
semmy-fit <71707190+semmy-fit@users.noreply.github.com>
2020-11-14 00:23:16 +0700

    lab4

---
+++ lab4.c
@@ -0,0 +1,92 @@
+#include <sys/types.h> /*POSIX*/
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <syslog.h>
+#include <stdlib.h>
+#define DEBUG
+
+int main(void){
+pid_t p;
+int fd;
+int operate;
+int result;
+int a;
+int b;
+printf("Original program, pid=%d\n", getpid());
+p=fork();
+if(p==0) {
+    printf("In child process, pid=%d, ppid=%d\n", getpid(), getppid());
+    a=10;
+    b=20;
+    operate=a+b;
+    result=operate;
+    printf("%d\n",result);
+    operate=b-a;
+    result=operate;
+     printf("%d\n",result);
+    operate=b/a;
+    result=operate;
+     printf("%d\n",result);
+    operate= b%2;
+    result=operate;
+     printf("%d\n",result);
+
+ 
+  
+} else {
+    printf("In parent, pid=%d, fork returned=%d\n", getpid(), p);
+       int d=15;
+    int c=2;
+    operate=d+c;
+    result=operate;
+     printf("%d\n",result);
+    operate=d-c;
+    result=operate;
+     printf("%d\n",result);
+    operate=d/c;
+    result=operate;
+     printf("%d\n",result);
+    operate= d%2;
+    result=operate;
+     printf("%d\n",result);
+}   
+
+                     int i=0;
+    openlog("test",LOG_PID,LOG_USER);
+
+#ifdef DEBUG
+    syslog(LOG_DEBUG,"try to sending 10 messages");
+#endif
+
+    for (i=0;i<10;i++){
+       int d=15;
+    int c=2;
+             operate=d+c;
+    result=operate;
+        syslog(LOG_INFO,"info message [result = %d] ",result);
+
+        FILE *fp = fopen("test23", "wb");
+    fputs("info message [result = %d]", fp);
+    fclose(fp);
+
+    fp = fopen("test", "r+b");
+    fseek(fp, 0, SEEK_SET);
+    fputs("abc", fp);
+    fclose(fp);
+
+                      
+    };
+
+#ifdef DEBUG
+    syslog(LOG_DEBUG,"try log to stderr");
+#endif
+    closelog();
+
+    openlog("test_stderr",LOG_PERROR | LOG_PID,LOG_USER);
+    syslog(LOG_INFO,"this is attempt to use stderr for syslog");
+    closelog();
+ 
+
+}
